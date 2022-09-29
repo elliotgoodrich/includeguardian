5bf9a78f5b7145d069273948c840b8f6a7f3a243
Elliot Goodrich <elliotgoodrich@gmail.com>
2022-09-29 08:24:20 +0100

    Take out CLI logic out of main.m.cpp
    Remove all the logic to its own component so that we are better able to
    write unit tests for it in the future.

---
+++ src/includeguardian.hpp
@@ -0,0 +1,12 @@
+#ifndef INCLUDE_GUARD_0B0B1CC2_10E3_40D7_8E84_6C5803D3E5ED
+#define INCLUDE_GUARD_0B0B1CC2_10E3_40D7_8E84_6C5803D3E5ED
+
+#include <iosfwd>
+
+namespace IncludeGuardian {
+
+int run(int argc, const char **argv, std::ostream& out, std::ostream& err);
+
+} // namespace IncludeGuardian
+
+#endif
